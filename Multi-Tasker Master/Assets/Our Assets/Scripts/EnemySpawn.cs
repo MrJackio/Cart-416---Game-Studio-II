f329915de6afa071454f82eb6bf510efbbbd6160
MrJackio <jackvanderslootharrison@gmail.com>
2019-03-04 08:40:31 -0500

    Finished Spawning Script

---
+++ Multi-Tasker Master/Assets/Our Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public float Timer = 2;
+    public GameObject enemy;
+    GameObject enemyClone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0f)
+        {
+            enemyClone = Instantiate(enemy, new Vector3(0, 1, 11), Quaternion.identity);
+            Timer = 2f;
+        }
+    }
+}
